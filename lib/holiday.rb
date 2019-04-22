4d10fa6fcea82af95189d84f7a0cfa7432089ad2
Francis Mendez <fmendez884@gmail.com>
2019-04-22 20:06:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,13 +74,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq = []
   holiday_hash.each do |key, value|
     value.each do |x|
       if x === "BBQ"
-        return x
+        bbq << x
       end
     end
-  end 
+  end
+  bbq
 end
 
 
