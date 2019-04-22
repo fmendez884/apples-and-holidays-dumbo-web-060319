e67ef7411ad012eac602a27444efa68fc7688666
Francis Mendez <fmendez884@gmail.com>
2019-04-22 20:07:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |key, value|
     value.each do |x|
       if x === "BBQ"
-        bbq << x
+        bbq.push(x)
       end
     end
   end
